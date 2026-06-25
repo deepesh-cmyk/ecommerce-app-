cc45207b0e1c5625cece4d27a2f0987ff3a261ab
Deepesh <deepesh@student.tce.edu>
2026-06-25 18:20:23 +0530

    update cors for production

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -11,7 +11,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://ecommerce-frontend.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
